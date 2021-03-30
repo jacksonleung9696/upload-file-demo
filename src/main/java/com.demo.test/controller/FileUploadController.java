3ea136bee056942e527223b2678d8493e0dc06c5
lianghuayue <liang_hy@hisuntech.com>
2021-03-31 00:42:29 +0800

    first commit

---
+++ src/main/java/com.demo.test/controller/FileUploadController.java
@@ -0,0 +1,17 @@
+package com.demo.test.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lianghuayue
+ * @Date 2021/3/30 23:40
+ * @Version 1.0
+ */
+@RestController("demo")
+public class FileUploadController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileUploadController.class);
+
+
+}
